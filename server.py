4ba990967934cd3f5043d746c55418ad9ab6d18a
MathiasDPX <mathias@dupeux.net>
2024-11-13 18:01:16 +0100

    Hash ip to sha256

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, jsonify
+from hashlib import sha256
 import json
 import os
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/get/", methods=["GET"])
 def get():
+    ip = sha256(request.remote_addr.encode('utf-8')).hexdigest()
     if not os.path.exists("data.json"): # If the file doesn't exist, create it and write empty dict
         with open("data.json", 'w') as file:
             file.write('{}')
@@ -14,24 +16,24 @@ def get():
     else:
         data = json.load(open("data.json", "r"))
 
-    return [data.get(request.remote_addr, 0)]
+    return [data.get(ip, 0)]
 
 @app.route("/set/", methods=["GET"])
 def set():
+    ip = sha256(request.remote_addr.encode('utf-8')).hexdigest()
     score = request.args.get("s", type=int, default=0)
 
     if not os.path.exists("data.json"): # If the file doesn't exist, create it and write empty dict
         with open("data.json", 'w') as file:
             file.write('{}')
-
     # Saving
     data = json.load(open("data.json", "r"))
 
-    if request.remote_addr in data.keys():
-        if score < data[request.remote_addr]:
+    if ip in data.keys():
+        if score < data[ip]:
             return {"success": False, "message": "Not a best score"}
         
-    data[request.remote_addr] = score
+    data[ip] = score
     json.dump(data, open("data.json", "w+"))
 
     return jsonify(success=True)
